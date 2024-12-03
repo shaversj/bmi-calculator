9f27b2f1e77783ce1a85a1b5c4d1624d6d36f4fe
Josh Shavers <shaversj@gmail.com>
2024-12-03 06:16:45 -0600

    Add basic html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
@@ -11,6 +13,37 @@ export default function Home() {
             </p>
           </div>
           <div>{/*  Calculator */}</div>
+        </div>
+
+        <div>
+          <h2>What your BMI result means</h2>
+          <p>
+            A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and
+            sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.
+          </p>
+        </div>
+
+        <div>
+          <div>
+            <Image src={"/images/icon-eating.svg"} alt={"Healthy eating"} width={64} height={64} />
+            <h3>Healthy eating</h3>
+            <p>Healthy eating promotes weight control, disease prevention, better digestion, immunity, mental clarity, and mood.</p>
+          </div>
+          <div>
+            <Image src={"/images/icon-exercise.svg"} alt={"Regular exercise"} width={64} height={64} />
+            <h3>Regular exercise</h3>
+            <p>Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity.</p>
+          </div>
+          <div>
+            <Image src={"/images/icon-sleep.svg"} alt={"Adequate sleep"} width={64} height={64} />
+            <h3>Adequate sleep</h3>
+            <p>Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation.</p>
+          </div>
+        </div>
+
+        <div>
+          <h4>Limitations of BMI</h4>
+          <p>Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial to use.</p>
         </div>
       </main>
     </div>
