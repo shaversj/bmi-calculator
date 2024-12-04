d92f4ba62002e809a09f49c4d6328be881fb1430
Josh Shavers <shaversj@gmail.com>
2024-12-04 13:22:51 -0600

    Add React Hook Form + Zod Resolver

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,40 @@
 "use client";
 
 import Image from "next/image";
-import { useState, useRef } from "react";
+import { useEffect } from "react";
+import { useForm, useWatch } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  height: z.number().min(1, "Height is required").gt(0),
+  weight: z.number().min(1, "Weight is required").gt(0),
+});
 
 export default function Home() {
-  const heightInputRef = useRef<HTMLInputElement>(null);
-  const weightInputRef = useRef<HTMLInputElement>(null);
-  const formRef = useRef<HTMLFormElement>(null);
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm({ resolver: zodResolver(schema) });
 
-  const [height, setHeight] = useState<string>("");
-  const [weight, setWeight] = useState<string>("");
-  const [bmi, setBmi] = useState<number>(0);
-
-  const handleHeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setHeight(e.target.value);
+  const onSubmit = (data: any) => {
+    const { height, weight } = data;
+    const bmi = (weight / (height * height)) * 10000;
+    console.log("BMI: ", bmi);
   };
 
-  const handleWeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setWeight(e.target.value);
-    if (height && e.target.value) {
-      setTimeout(() => {
-        e.preventDefault();
-        formRef.current?.submit();
-      }, 1000); // Delay of 1000 milliseconds (1 second)
+  const fieldValues = useWatch({
+    control: control,
+    name: ["height", "weight"], // Watch multiple fields
+  });
+
+  useEffect(() => {
+    if (isValid) {
+      handleSubmit(onSubmit)();
     }
-  };
-
-  const calculateBMI = (height: string, weight: string) => {
-    const heightNum = parseFloat(height);
-    const weightNum = parseFloat(weight);
-    if (heightNum > 0 && weightNum > 0) {
-      setBmi((weightNum / (heightNum * heightNum)) * 10000);
-    }
-  };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const height = heightInputRef.current?.value;
-    const weight = weightInputRef.current?.value;
-
-    if (height && weight) {
-      const heightNum = parseFloat(height);
-      const weightNum = parseFloat(weight);
-      if (heightNum > 0 && weightNum > 0) {
-        setBmi((weightNum / (heightNum * heightNum)) * 10000);
-        console.log("BMI: ", bmi);
-        formRef.current?.submit();
-      }
-    }
-  };
+  }, [isValid, fieldValues]); // Trigger effect on form validity and field changes
 
   return (
     <div>
@@ -57,21 +42,10 @@ export default function Home() {
         <div>
           <div>
             <h1>Body Mass Index Calculator</h1>
-            <p>
-              Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and
-              well-being.
-            </p>
+            <p>Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
           </div>
           <div>
-            {/*  Calculator */}
-            <form
-              id={"bmiForm"}
-              ref={formRef}
-              onSubmit={(e) => {
-                e.preventDefault();
-                calculateBMI(height, weight);
-              }}
-            >
+            <form>
               <div>
                 <div>
                   <label htmlFor={"metric"}>Metric</label>
@@ -85,16 +59,15 @@ export default function Home() {
 
               <div>
                 <label htmlFor="height">Height (cm):</label>
-                <input type="number" id="height" name="height" ref={heightInputRef} onChange={handleHeightChange} />
+                <input {...register("height", { valueAsNumber: true })} type="number" id="height" name="height" />
               </div>
               <div>
                 <label htmlFor="weight">Weight (kg):</label>
-                <input type="number" id="weight" name="weight" ref={weightInputRef} onChange={handleWeightChange} />
+                <input {...register("weight", { valueAsNumber: true })} type="number" id="weight" name="weight" />
               </div>
               <div>
-                {/*<h2>Welcome!</h2>*/}
-                {/*<p>Enter your height and weight and you’ll see your BMI result here</p>*/}
-                {bmi > 0 && <h2>Your BMI is: {bmi.toFixed(2)}</h2>}
+                <h2>Welcome!</h2>
+                <p>Enter your height and weight and you’ll see your BMI result here</p>
               </div>
             </form>
           </div>
@@ -102,10 +75,7 @@ export default function Home() {
 
         <div>
           <h2>What your BMI result means</h2>
-          <p>
-            A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and
-            sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.
-          </p>
+          <p>A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</p>
         </div>
 
         <div>
